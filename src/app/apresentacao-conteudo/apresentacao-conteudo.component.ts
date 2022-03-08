45b945b92f12760a64861d8249389752da9ab005
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-03-08 12:10:57 -0300

    event bind implementado

--- src/app/apresentacao-conteudo/apresentacao-conteudo.component.ts
+++ src/app/apresentacao-conteudo/apresentacao-conteudo.component.ts
@@ -5,27 +5,58 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './apresentacao-conteudo.component.html',
   styleUrls: ['./apresentacao-conteudo.component.css']
 })
-export class ApresentacaoConteudo implements OnInit {
 
-  valorAtual: string = '';
+/* 
+    ASSIM COMO É POSSIVEL FAZER O TEMPLATE LITERALS COM O 
+    DECORATOR "templateUrl", TAMBÉM É POSSIVEL FAZER O MESMO
+    COM O "styleUrls", COMO LOGO ABAIXO: 
 
+    styles:[
+       `
+        .hightlight {
+             background-color: yellow;
+             font-weight: bold;
+
+        }
+       `
+    ]
+
+*/
+
+
+
+
+export class ApresentacaoConteudo { // implements OnInit 
+/*
   ngOnInit(): void {
   }
+  */
+  valorAtual: string = '';
 
-  btnClick(evento: Event){ 
-      alert("Botao clicado!!!!!"); 
-  }
 
   inputClick(evento: KeyboardEvent){
-      console.log( 
+    /*
+    console.log( 
             (<HTMLInputElement> evento.target) 
             // CONVERTENDO UM OBJETO DO TIPO "EventTarget" para o tipo da interface "HTMLInputElement"
                  .value 
       );
+      */
 
       this.valorAtual = (<HTMLInputElement> evento.target).value;
       // CONVERTENDO UM OBJETO DO TIPO "EventTarget" para o tipo da interface "HTMLInputElement"
   }
 
+
+  mouseOut(valorCampo: any){
+       alert("mouse fora do elemento!!!!");
+       this.valorAtual = valorCampo;
+  }
+
+
+  btnClick(elementoInput: any){ 
+       alert("Botao clicado!!!"); 
+       console.dir( (<HTMLInputElement> elementoInput) );
+  }
 
 }
