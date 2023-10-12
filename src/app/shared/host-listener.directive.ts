74746511bc0f433ec7dadd3d049176d9fe1ba1be
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-10-11 23:53:05 -0300

    Adding first detection of Mouse Enter event

--- src/app/shared/host-listener.directive.ts
+++ src/app/shared/host-listener.directive.ts
@@ -1,10 +1,17 @@
 import { Directive, HostListener} from '@angular/core';
+// O HostListener permite 'escutar' um elemento HTML ao qual essa diretiva esta atrelada
 
 
 @Directive({
-  selector: '[appHostListener]'
+  selector: '[hostListener]'
 })
 export class HostListenerDirective {
+
+
+  @HostListener('mouseenter', ['isDragged']) 
+  mouseDragOverDetect( isDrag :string) :void{
+      alert("Host Listener detected a mouse dragged over!!!!  Valor do parametro recebido: "+isDrag);
+  }
 
   constructor() { 
 
