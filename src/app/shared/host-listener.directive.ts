e1227af291a886b04aa1f6401445585d1ab7cbb9
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-10-12 00:00:26 -0300

    adding ElemRef and Renderer2 to HostListener

--- src/app/shared/host-listener.directive.ts
+++ src/app/shared/host-listener.directive.ts
@@ -1,5 +1,5 @@
-import { Directive, HostListener} from '@angular/core';
-// O HostListener permite 'escutar' um elemento HTML ao qual essa diretiva esta atrelada
+import { Directive, HostListener, ElementRef, Renderer2} from '@angular/core';
+// O 'HostListener' permite 'escutar' um elemento HTML ao qual essa diretiva esta atrelada
 
 
 @Directive({
@@ -11,10 +11,16 @@ export class HostListenerDirective {
   @HostListener('mouseenter', ['isDragged']) 
   mouseDragOverDetect( isDrag :string) :void{
       alert("Host Listener detected a mouse dragged over!!!!  Valor do parametro recebido: "+isDrag);
+
+      this._rend.setProperty(
+         this._elemRef,
+         'display','block'
+      );
   }
 
-  constructor() { 
 
-  }
-
+  constructor(
+      private _elemRef: ElementRef,
+      private _rend :Renderer2
+  ){ }
 }
