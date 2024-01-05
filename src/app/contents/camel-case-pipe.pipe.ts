6ba3e52483ad23ecf03c9303ed5c93b6994d18a0
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-01-04 23:59:05 -0300

    adding new comments

--- src/app/contents/camel-case-pipe.pipe.ts
+++ src/app/contents/camel-case-pipe.pipe.ts
@@ -1,10 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+
+
 @Pipe({
-  name: 'camelCasePipe'
+  name: 'camelCasePipe' //o atributo 'name' é equivalente à um 'selector' declarado nos 'components'
 })
 export class CamelCasePipePipe implements PipeTransform {
 
+  // metodo responsavel por recebero valor 'value' e os argumentos para a formatação do valor recebido, podendo retornar qualquer tipo de objeto, por isso o 'unknown'
   transform(value: unknown, ...args: unknown[]): unknown {
     return null;
   }
