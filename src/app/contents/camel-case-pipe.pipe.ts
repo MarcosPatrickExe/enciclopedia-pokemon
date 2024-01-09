ef2155a3c7bad77e7d0a2a0eab00b18bf6d7edad
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-01-08 23:56:35 -0300

    new local variable

--- src/app/contents/camel-case-pipe.pipe.ts
+++ src/app/contents/camel-case-pipe.pipe.ts
@@ -9,9 +9,9 @@ export class CamelCasePipePipe implements PipeTransform {
 
     // 'transform' eh o metodo sobrescrito da interfacce 'PipeTransform'
     // metodo responsavel por recebero valor 'value' e os argumentos para a formatação do valor recebido, podendo retornar qualquer tipo de objeto, por isso o 'unknown'
-    transform(value: unknown, ...args: unknown[]): unknown {
+    transform( value: any, ...args: unknown[] ): unknown {
     //   let val = value.split;
-        
+         let values = value.split(' ');
          return null; // por padrao o pipe retorna o valor 'null'
     }
 
