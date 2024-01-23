752e9f419f357c595234764a350e1e5375755775
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-01-22 23:30:14 -0300

    add indenting into pipe class

--- src/app/contents/home-menu/pipe-filter-array.pipe.ts
+++ src/app/contents/home-menu/pipe-filter-array.pipe.ts
@@ -5,18 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PipeFilterArrayPipe implements PipeTransform {
 
-  transform(value: any, ...args: any): unknown {
+    transform(value: any, ...args: any): unknown {
 
-    if(value.length===0 || args == undefined){
-       return value;
+        if(value.length===0 || args == undefined){
+            return value;
+        }
+
+        let filterKey = args.toLowerCase();
+
+        // fazendo a busca de 'filterKey'
+        return value.filter(
+            (currValue :any) => (currValue.indexOf(filterKey) != -1)
+        );
     }
-
-    let filterKey = args.toLowerCase();
-
-    // fazendo a busca de 'filterKey'
-    return value.filter(
-        (currValue :any) => (currValue.indexOf(filterKey) != -1)
-    );
-  }
 
 }
