1e9a461736bd82c92db0f481054dc593850ca2df
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-03-03 00:47:06 -0300

    clearing comments (part 2)

--- src/app/contents/view-pokemon/pipe-filter-array.pipe.ts
+++ src/app/contents/view-pokemon/pipe-filter-array.pipe.ts
@@ -9,7 +9,7 @@ export class PipeFilterArrayPipe implements PipeTransform {
 
     transform(value: any, ...args: any): unknown[] {
 
-        if(value.length===0 || args == undefined){
+        if(value.length === 0 || args == undefined){
             return value;
         }
 
