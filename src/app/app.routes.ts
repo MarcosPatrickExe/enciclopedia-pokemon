77812c0cc3ea24f04efded6ffa2f0209b384039f
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-03-10 00:25:39 -0300

    adding the first route Home for website!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { ModuleWithProviders } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
 
 
 const ROUTES: Routes = [
     {  path:'', component: AppComponent},
-    {  path:'', component: AppComponent}
+    {  path:'home', component: HomeComponent}
 ]
 
 const Routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot( ROUTES ) ;
