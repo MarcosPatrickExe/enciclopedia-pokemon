6140c747051d739a93f3840aa7a04654abc5c695
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-03-17 14:32:18 -0300

    adding new route to About path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AppComponent } from './app.component';
 import { ModuleWithProviders } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeMenuComponent } from './contents/home-menu/home-menu.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
 const ROUTES: Routes = [
     { path:'', component: HomeComponent },
-    { path:'pokedex', component: HomeMenuComponent }
+    { path:'pokedex', component: HomeMenuComponent },
+    { path:'about', component: AboutComponent }
 ]
 
 const Routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot( ROUTES ) ;
